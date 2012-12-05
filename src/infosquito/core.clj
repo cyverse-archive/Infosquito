08fd3d1e913a9d46767d4a5a6a4df73c1059de8e
Tony Edgin <tedgin@iplantcollaborative.org>
2012-12-05 11:37:18 -0700

    Adapted code to use clojure-commons for config

--- src/infosquito/core.clj
+++ src/infosquito/core.clj
@@ -7,9 +7,8 @@
             [com.github.drsnyder.beanstalk :as beanstalk]
             [slingshot.slingshot :as ss]
             [clj-jargon.jargon :as irods]
-            [clojure-commons.clavin-client :as zk]
+            [clojure-commons.config :as config]
             [clojure-commons.infosquito.work-queue :as queue]
-            [clojure-commons.props :as props]
             [infosquito.es :as es]
             [infosquito.worker :as worker])
   (:import [java.net URL]))
@@ -31,6 +30,14 @@
               (get props "infosquito.irods.default-resource")))
 
 
+(defn- mk-es-url
+  [props]
+  (str (URL. "http"
+             (get props "infosquito.es.host")
+             (get-int-prop props "infosquito.es.port")
+             "")))
+
+
 (defn- mk-queue
   [props]
   (let [port (get-int-prop props "infosquito.beanstalk.port")
@@ -42,58 +49,18 @@
                      (get props "infosquito.beanstalk.tube"))))
 
 
-(defn- mk-es-url
-  [props]
-  (str (URL. "http"
-             (get props "infosquito.es.host")
-             (get-int-prop props "infosquito.es.port")
-             "")))
-
-
-(defn- log-config
-  [props]
-  (dorun (map (fn [[k v]] (log/warn "CONFIG:" k "=" v)) (sort-by key props))))
-
-
-(defn- run
-  "Throws:
-     :connection - This is thrown if it loses its connection to beanstalkd.
-     :connection-refused - This is thrown if it can't connect to Elastic Search.
-     :internal-error - This is thrown if there is an error in the logic error
-       internal to the worker.
-     :unknown-error - This is thrown if an unidentifiable error occurs.
-     :beanstalkd-oom - This is thrown if beanstalkd is out of memory."
-  [mode props]
-  (log-config props)
-  (let [worker (worker/mk-worker (init-irods props)
-                                 (mk-queue props)
-                                 (es/mk-indexer (mk-es-url props))
-                                 (get props "infosquito.irods.index-root")
-                                 (get props "infosquito.es.scroll-ttl")
-                                 (get-int-prop props
-                                               "infosquito.es.scroll-page-size"))]
-    (condp = mode
-      :sync   (worker/sync-index worker)))
-      :worker (dorun (repeatedly #(worker/process-next-task worker))))
-
-
-(defn- get-local-props
+(defn- load-props
   [cfg-file]
   (ss/try+
-    (props/read-properties cfg-file)
+    (let [props (ref nil)]
+      (if cfg-file
+        (config/load-config-from-file nil cfg-file props)
+        (config/load-config-from-zookeeper props "infosquito"))
+      @props)
     (catch Object _
-      (ss/throw+ {:type :cfg-problem :cfg-file cfg-file}))))
-
-
-(defn- get-remote-props
-  []
-  (let [zkprops (props/parse-properties "zkhosts.properties")]
-    (zk/with-zk (get zkprops "zookeeper")
-      (if (zk/can-run?)
-        (zk/properties "infosquito")
-        (ss/throw+ {:type :zk-perm})))))
-
-
+      (ss/throw+ {:type :cfg-problem}))))
+        
+  
 (defn- map-mode
   [mode-str]
   (condp = mode-str
@@ -118,26 +85,42 @@
       (ss/throw+ {:type :cli :msg (.getMessage e)}))))
 
 
+(defn- run
+  "Throws:
+     :connection - This is thrown if it loses its connection to beanstalkd.
+     :connection-refused - This is thrown if it can't connect to Elastic Search.
+     :internal-error - This is thrown if there is an error in the logic error
+       internal to the worker.
+     :unknown-error - This is thrown if an unidentifiable error occurs.
+     :beanstalkd-oom - This is thrown if beanstalkd is out of memory."
+  [mode props]
+  (config/log-config props)
+  (let [worker (worker/mk-worker (init-irods props)
+                                 (mk-queue props)
+                                 (es/mk-indexer (mk-es-url props))
+                                 (get props "infosquito.irods.index-root")
+                                 (get props "infosquito.es.scroll-ttl")
+                                 (get-int-prop props
+                                               "infosquito.es.scroll-page-size"))]
+    (condp = mode
+      :sync   (worker/sync-index worker)))
+      :worker (dorun (repeatedly #(worker/process-next-task worker))))
+
+
 (defn -main
   [& args]
   (ss/try+
     (let [[opts _ help-str] (parse-args args)]
       (if (:help opts)
         (println help-str)
-        (run (map-mode (:mode opts))
-             (if-let [cfg-file (:config opts)]
-               (get-local-props cfg-file)
-               (get-remote-props)))))
+        (run (map-mode (:mode opts)) (load-props (:config opts)))))
     (catch [:type :cli] {:keys [msg]}
       (log/error (str "There was a problem reading the command line input. ("
                       msg ") Exiting.")))
     (catch [:type :invalid-mode] {:keys [mode]}
       (log/error "Invalid mode, " mode))
-    (catch [:type :cfg-problem] {:keys [cfg-file]}
-      (log/error str("There was problem reading the configuration file "
-                      cfg-file ". Exiting.")))
-    (catch [:type :zk-perm] {:keys []}
-      (log/error "This application cannot run on this machine. Exiting."))
+    (catch [:type :cfg-problem] {:keys []}
+      (log/error "There was problem loading the configuration values.  Exiting."))
     (catch [:type :connection-refused] {:keys [msg]}
       (log/error (str "Cannot connect to Elastic Search. (" msg ") Exiting.")))
     (catch [:type :connection] {:keys [msg]}
