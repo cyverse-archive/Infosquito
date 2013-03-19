9a5757d0faab5349c81700780065a298c7704e24
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-19 15:13:03 -0700

    upped dependencies and increased version to 0.2.1-SNAPSHOT
    clojure -> 1.5
    cheshire -> 5.0.2
    jargon-core -> 3.3
    clj-jargon -> 0.2.4-SNAPSHOT
    clojure-commons -> 1.4.1-SNAPSHOT
    boxy -> 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject infosquito "0.2.0-SNAPSHOT"
+(defproject infosquito "0.2.1-SNAPSHOT"
   :description "This is a web service to enable searching of iPlant Collaborative user content."
   :license {:url "file://LICENSE.txt"}
   :aot [infosquito.core]
   :main infosquito.core
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/tools.logging "0.2.4"]
-                 [cheshire "5.0.1"]
+                 [cheshire "5.0.2"]
                  [clojurewerkz/elastisch "1.0.2"]
                  [com.github.drsnyder/beanstalk "1.0.0-clj14"]
-                 [org.irods.jargon/jargon-core "3.2.1.3"]
+                 [org.irods.jargon/jargon-core "3.3.0"]
                  [slingshot "0.10.3"]
-                 [org.iplantc/clj-jargon "0.2.2-SNAPSHOT"]
-                 [org.iplantc/clojure-commons "1.4.0-SNAPSHOT"]]
-  :profiles {:dev {:dependencies   [[org.iplantc/boxy "0.1.1-SNAPSHOT"]]
+                 [org.iplantc/clj-jargon "0.2.4-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.4.1-SNAPSHOT"]]
+  :profiles {:dev {:dependencies   [[org.iplantc/boxy "0.1.2-SNAPSHOT"]]
                    :resource-paths ["dev-resources"]}}
   :plugins [[org.iplantc/lein-iplant-rpm "1.4.0-SNAPSHOT"]]
   :iplant-rpm {:summary      "infosquito"
