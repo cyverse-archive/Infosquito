a3fe7b416a1d04b99723c42f074dfc0f9c3f659a
Dennis Roberts <dennis@iplantcollaborative.org>
2014-03-04 10:30:14 -0700

    bumped the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.iplantc/infosquito "1.8.6-SNAPSHOT"
+(defproject org.iplantc/infosquito "1.8.8-SNAPSHOT"
   :description "An ICAT database crawler used to index the contents of iRODS."
   :url "http://www.iplantcollaborative.org"
   :license {:name "BSD"
